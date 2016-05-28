db03beb8a7ff246a0a7aaf24b826e56ade8182ab
Mariah Tree <mariah.tree@gmail.com>
2016-05-28 14:50:03 -0500

    items populate dynamically

--- js/listingResults.js
+++ js/listingResults.js
@@ -1,1 +1,21 @@
 var $ = require("jquery");
+
+var $listings = $("[data-js='listings']");
+
+this.appendResults =function(){
+
+  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?", function(data){
+  console.log(data);
+    data.results.forEach(function(resp) {
+      $listings.append(`
+        <span class="grid__col--3 photo__wrapper" >
+          <img class="photo__el" src="${resp.Images[0].url_170x135}"/>
+          <h2 class="result__caption">${resp.title}</h2>
+          <h2 class="shop-name">${resp.Shop.shop_name}</h2>
+          <h2 class="price">${resp.price}</h2>
+        </span>
+        `)
+    })
+});
+
+}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 var $ = require("jquery");
-var $ = require("listingResults")
+var listingResults = require("listingResults");
+
 
 $(function(){
-
-  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images&callback=?", function(data){
-  console.log(data);
-});
-
- etsyData.forEach(function(listing){
-    listingResults.appendResults(listing);
-   });
-
+  listingResults.appendResults();
 })
